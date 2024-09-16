aaafb7ab09aeda512a8b41849c7063e7ca179e05
Binary <binaris000@gmail.com>
2024-09-16 11:22:00 -0400

    Init mod

---
+++ src/client/java/binaris/exploration_revamped/Exploration_revampedClient.java
@@ -0,0 +1,10 @@
+package binaris.exploration_revamped;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Exploration_revampedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/binaris/exploration_revamped/Exploration_revampedDataGenerator.java
@@ -0,0 +1,11 @@
+package binaris.exploration_revamped;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Exploration_revampedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
